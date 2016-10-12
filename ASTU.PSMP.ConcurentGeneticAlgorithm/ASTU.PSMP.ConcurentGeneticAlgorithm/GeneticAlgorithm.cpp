4a33e09a14d5761593bbdd1159d849a5d3b2831e
VKorney <vitaliy.korney@enterra-inc.com>
2016-10-12 23:11:33 +0700

    added NaturalSelect

--- ASTU.PSMP.ConcurentGeneticAlgorithm/ASTU.PSMP.ConcurentGeneticAlgorithm/GeneticAlgorithm.cpp
+++ ASTU.PSMP.ConcurentGeneticAlgorithm/ASTU.PSMP.ConcurentGeneticAlgorithm/GeneticAlgorithm.cpp
@@ -62,7 +62,7 @@ void GeneticAlgorithm::Mutate()
 {
 	for (int i = 0; i < population.size(); i++)
 	{
-		if (rand() / RAND_MAX < geneticParameters->GetMutationProbability) 
+		if (rand() / RAND_MAX < geneticParameters->GetMutationProbability()) 
 		{
 			auto originalOrganism = population.at(i);
 			auto mutantOrganism = ProduceMutant(originalOrganism);
@@ -89,10 +89,32 @@ struct inverseOrderStruct {
 void GeneticAlgorithm::NaturalSelect()
 {
 	std::vector<int> notFightedYet;
-	
+	for (int i = 0; i < population.size(); i++)
+	{
+		notFightedYet.push_back(i);
+	}
 	std::set<int,inverseOrderStruct> diedOrganism;
 	for (int i = 0; i < geneticParameters->GetReproductionNumber()*2; i++)
 	{
+		auto fighter1SelectorIndex = rand() % notFightedYet.size();
+		auto fighter1Index = notFightedYet.at(fighter1SelectorIndex);
+		auto fighter1 = population.at(fighter1Index);
+		notFightedYet.erase(notFightedYet.begin() + fighter1SelectorIndex);
 
+		auto fighter2SelectorIndex = rand() % notFightedYet.size();
+		auto fighter2Index = notFightedYet.at(fighter2SelectorIndex);
+		auto fighter2 = population.at(fighter2Index);
+		notFightedYet.erase(notFightedYet.begin() + fighter2SelectorIndex);
+
+		int deathIndex;
+		if (rand() % RAND_MAX < geneticParameters->GetBadOrganizmDeathProbability())
+		{
+			deathIndex = (MeasureFitness(fighter1) < MeasureFitness(fighter2)) ? fighter1Index : fighter2Index;
+		}
+		else
+		{
+			deathIndex = (MeasureFitness(fighter1) < MeasureFitness(fighter2)) ? fighter2Index : fighter1Index;
+		}
+		population.erase(population.begin() + deathIndex);
 	}
 }
